ebb8b8a1fb29a34e860cfaadad2c19c0cfa0e44a
Alexander1988 <ultramagnus88@gmail.com>
2021-11-11 01:47:31 +0300

    added postgres

--- myavesomeblog/settings.py
+++ myavesomeblog/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'myavesomeblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myawesomeblogdb',
+        'USER': 'postgres',
+        'PASSWORD':'blog1234',
+        'HOST':'localhost',
+        'PORT': '5432',
+
     }
 }
 
