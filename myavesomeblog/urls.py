1c3371d768b4390e596fb9b13942f80d7692d423
Alexander1988 <ultramagnus88@gmail.com>
2021-11-11 02:14:11 +0300

    home html page was added

--- myavesomeblog/urls.py
+++ myavesomeblog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import events.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', events.views.home, name='home'),
 ]
